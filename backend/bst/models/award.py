5525c8123471fc27eff3671a75114b0ba3fae372
vikik371 <2276877@gnkhalsa.edu.in>
2025-02-04 00:36:10 +0530

    update in models

---
+++ backend/bst/models/award.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# from accounts.models import 
+
+# class Award(models.Model):
+#     title = models.CharField(max_length=255, blank=True, null=True)
+#     description = models.TextField(blank=True, null=True)
+#     user = models.ForeignKey(User, related_name="user_award", on_delete=models.CASCADE)
+#     awarded_by = models.ForeignKey(User, related_name="awarded_by_user", on_delete=models)
+#     awarded_at = models.DateField(default='01-01-2025')
