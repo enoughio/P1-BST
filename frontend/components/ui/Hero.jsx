9563b7216b78a0885cf687228dacf18508b272b4
Ani <turboji007@gmail.com>
2025-02-07 18:36:38 +0530

    started landing page

---
+++ frontend/components/ui/Hero.jsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import { FaInstagram, FaFacebookF } from "react-icons/fa";
+import { AiOutlineYoutube } from "react-icons/ai";
+import { CiLinkedin } from "react-icons/ci";
+
+
+export default function HeroSection() {
+  return (
+    <section className="flex flex-col md:flex-row w-full h-screen
+    ">
+      hero
+    </section>
+  )
+}
+
+
+
+
+{
+  /*
+    <div className="relative">
+<div className="relative aspect-square overflow-hidden [border-radius:48px_48px_48px_150px] bg-pink-100">
+<Image
+    src="/test.svg" // 🔹 Use actual image path
+    alt="Hero image"
+    fill
+    className="object-cover bg-white"
+    priority
+  />
+</div>
+<div className="absolute -left-4 top-1/3 flex -space-x-4">
+<Image
+    src="/kid-playing.jpg" // 🔹 Use actual image path
+    alt="Profile 1"
+    width={64}
+    height={64}
+    className="h-16 w-16 rounded-full border-2 border-white bg-white"
+  />
+  <Image
+    src="/happy-speach.jpg" // 🔹 Use actual image path
+    alt="Profile 2"
+    width={64}
+    height={64}
+    className="h-16 w-16 rounded-full border-2 border-white bg-white"
+  />
+</div>
+</div>
+   */
+}
