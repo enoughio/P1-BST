0c07634092aa5f51f0daad88f8e4167e07724a05
Ani <turboji007@gmail.com>
2025-02-17 00:32:43 +0530

    refactor JoinUs component; separate MemberCard and ClubCard; update styles in Journy and Testimonials components

--- frontend/components/Journy.jsx
+++ frontend/components/Journy.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const StatCard = ({ number, label1, label2 }) => {
   return (
-    <div className="relative w-40 h-40 bg-red-100 rounded-full flex justify-center items-center">
+    <div className="relative w-44 h-44 bg-red-100 rounded-full flex justify-center items-center">
       <div className="absolute -top-2 -right-4 bg-red-300 w-16 h-16 rounded-full flex justify-center items-center">
         +
       </div>
@@ -17,7 +17,7 @@ const StatCard = ({ number, label1, label2 }) => {
 
 const Journy = () => {
   return (
-    <div className="bg-red-200 w-full h-[35vh] flex justify-center gap-24 items-center my-5">
+    <div className="bg-red-200 w-full h-[40vh] flex justify-center gap-24 items-center my-5">
       <StatCard number="200" label1="Happy" label2="Members" />
       <StatCard number="100+" label1="Successful" label2="Meetings" />
       <StatCard number="8" label1="Years of" label2="Experience" />
--- frontend/components/ui/Testimonials.jsx
+++ frontend/components/ui/Testimonials.jsx
@@ -28,17 +28,17 @@ const testimonials = [
 
 const Testimonials = () => {
   return (
-    <div className="flex-col flex justify-center items-center ">
+    <div className="flex-col flex justify-center items-center  bg-blue-100 py-10">
       <div className="self-center text-center mb-5">
-        <h1>Testimonials</h1>
+        <h1 className="font-semibold text-2xl">Testimonials</h1>
         <div>This what our Members say about us</div>
       </div>
 
       {/* cards */}
-      <div className="flex flex-row  justify-center items-center gap-2 mx-10">
+      <div className="flex flex-row  justify-center items-center gap-4 mx-10">
         {testimonials.map((testimonial, indx) => (
           // card
-          <div key={indx} className="py-5 w-[90%] md:w-1/3 flex justify-center items gap-1 border-2 rounded-2xl overflow-hidden">
+          <div key={indx} className="py-5 bg-white w-[90%] md:w-1/3 flex justify-center items gap-1 backdrop-blur-xl rounded-2xl overflow-hidden">
             <div className="w-[50%] h-full flex flex-col justify-center items-center gap-3">
               <div className="w-full h-auto">
                 <Image
