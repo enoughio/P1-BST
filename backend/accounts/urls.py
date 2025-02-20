e3ffe013ce0aafac82abd95a13f9934bea4d4b55
vikik371 <2276877@gnkhalsa.edu.in>
2025-02-20 18:49:57 +0530

    add

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -4,12 +4,9 @@ from .views import (RegisterAPIView,
                     LoginAPIView,
                     LogoutAPIView,
     
-<<<<<<< HEAD
                     MemberRetriveAPIView,
                     MemberListAPIView,
-=======
                     MemberAPIView,
->>>>>>> 4312131207f86003a5d39219bf7db4df82b05cd7
                     MemberRetrieveUpdateDestroyAPIView,
                     
                     MemberProjectRetrieveUpdateAPIView,
@@ -21,7 +18,6 @@ from .views import (RegisterAPIView,
 urlpatterns = [
     path("login/", LoginAPIView.as_view(), name="login"),
     path("logout/", LogoutAPIView.as_view(), name="logout"),
-<<<<<<< HEAD
     path('register/', RegisterAPIView.as_view(), name='register'),
 
     path('<str:username>/dashboard/', MemberRetriveAPIView.as_view(), name='get-member-dashboard'),
@@ -34,7 +30,6 @@ urlpatterns = [
     path('<str:username>/dashboard/', AdminRetrieveUpdateDestroyAPIView.as_view(), name='get-admin'),
     path('admins/', AdminAPIView.as_view(), name='create-admin'),
     path('admins/', AdminAPIView.as_view(), name='list-admin'),
-=======
     path('register/', RegisterAPIView.as_view(), name='register-member'),
 
     path('members/', MemberAPIView.as_view(), name='list-member'),
@@ -47,7 +42,6 @@ urlpatterns = [
     path('admins/', AdminAPIView.as_view(), name='create-admin'),
     path('admins/', AdminAPIView.as_view(), name='list-admin'),
     path('admins/<str:username>/', AdminRetrieveUpdateDestroyAPIView.as_view(), name='get-admin'),
->>>>>>> 4312131207f86003a5d39219bf7db4df82b05cd7
     path('admins/<str:username>/', AdminRetrieveUpdateDestroyAPIView.as_view(), name='update-admin'),
     path('admins/<str:username>/', AdminRetrieveUpdateDestroyAPIView.as_view(), name='remove-admin'),
 ]
