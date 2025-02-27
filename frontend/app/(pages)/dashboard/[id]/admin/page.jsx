1f1b6777c4ddf2da6e673d98c3e91f809eadbb0e
Ani <turboji007@gmail.com>
2025-02-27 12:27:07 +0530

    added club details componet for admin pannel

--- frontend/app/(pages)/dashboard/[id]/admin/page.jsx
+++ frontend/app/(pages)/dashboard/[id]/admin/page.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import ClubDetails from '@/components/ui/dashboard/admin/ClubDetails'
+import React, { use } from 'react'
+
+// TODO: fetch data of admin of the club and pass that admin name to club detaisl component
+
+const adminData = {
+  name: 'John Doe',
+  username: 'johndoe',
+  email: "jhon@example.com",
+  isStaff: 'admin',
+  clubId: "21A",
+  clubName: "Club Name",
+}
+
 
 const page = () => {
   return (
     <div>
-        
-        
 
+      <ClubDetails  clubId={adminData.clubId} />
+        
 
     </div>
   )
