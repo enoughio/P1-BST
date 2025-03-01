7cfd89bd45f9039b221bc388bf38dbf1da183326
Ani <turboji007@gmail.com>
2025-03-02 00:12:15 +0530

    changed imports

--- frontend/components/ui/dashboard/admin/ClubDetails.jsx
+++ frontend/components/ui/dashboard/admin/ClubDetails.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { fetchClubData } from '@/utils/club.js'
+// import { fetchClubData } from '@/utils/club.js'
 
 // TODO: fetch data of admin of the club and pass that admin name to club detaisl component
 
 const ClubDetails = ({props}) => {
 
-    const {clubId} = props
-    const clubData = fetchClubData(clubId)
+    // const {clubId} = props
+    // const clubData = fetchClubData(clubId)
 
   return (
     <div>ClubDetails</div>
