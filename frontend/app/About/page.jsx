c4a2654a1768602a596fb6b291cd6d262e2ae015
Ani <turboji007@gmail.com>
2025-03-09 10:29:21 +0530

    changed imports at about page

--- frontend/app/About/page.jsx
+++ frontend/app/About/page.jsx
@@ -1,10 +1,8 @@
-import Fotter from '@/components/Fotter'
-import Header from "../../components/ui/About/Header.jsx"
-import AboutUs from '../../components/ui/About/AboutUs.jsx'
-import OurStory from '../../components/ui/About/OurStory.jsx'
-import Aprosal from '../../components/ui/About/Aprosal.jsx'
-import Mission from '../../components/ui/About/Mission.jsx'
-import Head from 'next/head'
+import Header from '@/components/ui/About/Header.jsx' 
+import AboutUs from '@/components/ui/About/AboutUs.jsx'
+import OurStory from '@/components/ui/About/OurStory.jsx'
+import Aprosal from '@/components/ui/About/Aprosal.jsx'
+import Mission from '@/components/ui/About/Mission'
 import React from 'react'
 
 // TODO: there is an hydration error in this page that needs to be fixed 
