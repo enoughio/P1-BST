bfee9abf2ff2c9229b01d43f77858242d73472d2
vikik371 <2276877@gnkhalsa.edu.in>
2025-03-21 14:12:00 +0530

    file moved from local to remote area

--- bst/models/project.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Project(models.Model):
-    project_id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=255, blank=False, null=False)
-    description = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.title
