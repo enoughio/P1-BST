07b6693de2b0b5dc16bc211329f26b2feef9a793
Ani <turboji007@gmail.com>
2025-04-03 23:27:55 +0530

    deploying

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,20 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'picsum.photos',
-                pathname: '/**',
-            }
-        ]
-    },
-
-
-    
-
-
 };
 
 export default nextConfig;
